d49fd5e809ee5b3bb961731a24930fc2bc0e3f4a
leonardocarranza16 <112891280+leonardocarranza16@users.noreply.github.com>
2023-06-02 20:09:55 -0600

    Creation controller and Routers Authors y the code file Api for Authors

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $author = Author::orderBy('full_name', 'asc')->get();
+        return response()->json($author);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $author = new Author();
+            $author->full_name = $request->full_name;
+            $author->birth_date = $request->birth_date;
+            $author->country = $request->country;
+            $author->save();
+            return response()->json(['status' => true, 'message' => 'El autor ' . $author->full_name . ' fue creado exitosamente. ']);
+        } catch (\Exception $exc) {
+            return response()->json(['status' => false, 'message' => 'Error al crear el registro ' . $exc]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $author = Author::find($id);
+        return response()->json($author);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $author = Author::findOrFail($id);
+            $author->full_name = $request->full_name;
+            $author->birth_date = $request->birth_date;
+            $author->country = $request->country;
+            $author->save();
+            return response()->json(['status' => true, 'message' => 'El author ' . $author->full_name . ' fue actualizado correctamente']);
+        } catch (\Exception $exc) {
+            return response()->json(['status' => false, 'message' => 'Error al editar el registro ' . $exc]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $author = Author::findOrFail($id);
+            $author->delete();
+            if ($author->image()) {
+                $author->image()->delete();
+            }
+            return response()->json(['status' => true, 'message' => 'Elautor ' . $author->full_name . ' fue eliminado exitosamente']);
+        } catch (\Exception $exc) {
+            return response()->json(['status' => false, 'message' => 'Error al eliminar el registro ' . $exc]);
+        }
+    }
+}
