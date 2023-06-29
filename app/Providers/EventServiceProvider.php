67137c8324599be37824fbdfb7dfaff2ca236f57
leonardocarranza16 <112891280+leonardocarranza16@users.noreply.github.com>
2023-06-29 16:41:03 -0600

    Creacion de un observable para borrar la imagen cuando elimina el autor

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+Use App\Models\Author;
+Use App\Observers\AuthorObserver;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,7 +29,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Author::observe(AuthorObserver::class);
+        // Book::observe(BookObserver::class);
     }
 
     /**
